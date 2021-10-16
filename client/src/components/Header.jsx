79733662125dc0514199b6cbfafb7fd5aea3ec8f
Raunak Singh <rksj2001@gmail.com>
2021-10-16 15:49:34 +0530

    Created Navigation

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { AppBar, Toolbar, ButtonGroup, Button, Box } from "@mui/material";
+
+import logo from "../assets/images/IMG_20211014_172013_181-removebg-preview.png";
+
+import Navigation from "./Navigation";
 
 const Header = () => {
-    return(
-        <div className="header">
-            Header
-        </div>
-    )
+  return (
+    <AppBar position="static" color="inherit" gutterBottom>
+      <Toolbar variant="regular">
+        <img src={logo} alt="Logo" height="100px" />
+        <Box sx={{ flexGrow: 0.9 }}></Box>
+        <Navigation />
+      </Toolbar>
+    </AppBar>
+  )
 }
 
 export default Header;
--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme, ThemeProvider, responsiveFontSizes } from '@mui/material/styles';
 
 import App from './App';
 
-const theme = createTheme({
-    palette: {
-        primary: {
-            main: "#F23E16"
-        },
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: "#F23E16"
+    },
 
-        secondary: {
-            main: "#F2AA52"
-        }
-    }
-})
+    secondary: {
+      main: "#F2AA52"
+    },
+  },
+});
+
+theme = responsiveFontSizes(theme);
 
 ReactDOM.render(
-    <ThemeProvider theme={theme}>
-        <App />
-    </ThemeProvider>
-    ,document.getElementById('root'));
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+  , document.getElementById('root'));
