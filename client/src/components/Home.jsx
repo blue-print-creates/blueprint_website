fc93f9790a15e829130f9d53b859773c444db025
Rakshit Gupta <75110126+RakshitGupta621@users.noreply.github.com>
2021-10-17 08:05:09 +0530

    Add files via upload

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Typography } from "@mui/material";
-
+import { Typography, Card, CardActionArea, CardContent, CardMedia, Grid, Container, Button} from "@mui/material";
+import Slider from "./Slider"
+const cards = [1, 2, 3];
 // Home Page
 // -Intro Video
 // -Photo Slider
@@ -9,14 +10,38 @@ import { Typography } from "@mui/material";
 const Home = () => {
     return(
         <div className="home">
-            <Typography variant="h1" color="primary">
-                Home Page
-            </Typography>
-            <Typography variant="h2" color="secondary">
-                Welcome
-            </Typography>
+            <Container maxWidth="lg" style={{marginTop:'100px'}}>
+                <Container maxWidth="lg" style={{border: "25px solid orange", marginBottom: "40px"}}>
+                    <Slider/>
+                </Container>
+
+                <Grid container spacing={4}>
+                    {cards.map((card) => (
+                        <Grid item key={card} xs={12} sm={6} md={4}>
+                            <Card sx={{ maxWidth: 345 }}>
+                            <CardActionArea>
+                                <CardMedia
+                                component="img"
+                                height="240"
+                                image="https://source.unsplash.com/1280x720/?lamborghini'"
+                                alt="Cars"
+                                />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Cars
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    The first production car with a recorded speed was the 1894 Benz Velo that had a top speed of just 12 mph. 
+                                </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
         </div>
     )
 }
 
-export default Home;
+export default Home; 
---
+++ client/src/components/Slider.jsx
@@ -0,0 +1,128 @@
+import * as React from 'react';
+import { useTheme, Box, MobileStepper, Paper, Typography, Button} from "@mui/material";
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = [
+  {
+    label: 'San Francisco – Oakland Bay Bridge, United States',
+    imgPath:
+      'https://source.unsplash.com/1280x720/?cars',
+  },
+  {
+    label: 'Bird',
+    imgPath:
+      'https://source.unsplash.com/1280x720/?mechanics',
+  },
+  {
+    label: 'Bali, Indonesia',
+    imgPath:
+      'https://source.unsplash.com/1280x720/?tesla',
+  },
+  {
+    label: 'Goč, Serbia',
+    imgPath:
+      'https://source.unsplash.com/1280x720/?ferrari',
+  },
+];
+
+function SwipeableTextMobileStepper() {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = images.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
+  return (
+    <Box lg={{ maxWidth: 400, flexGrow: 1 }}>
+      <Paper
+        square
+        elevation={0}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          height: 40,
+          pl: 2,
+          bgcolor: 'background.default',
+        }}
+      >
+        <Typography>{images[activeStep].label}</Typography>
+      </Paper>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {images.map((step, index) => (
+          <div key={step.label}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  height: 600,
+                  display: 'block',
+                  maxWidth: 1280,
+                  overflow: 'hidden',
+                  borderShadow: 1,
+                  backgroundColor: 'primary.light',
+                  '&:hover': {
+                    backgroundColor: 'primary.main',
+                      opacity: [0.9, 0.8, 0.7],
+                    }
+                }}
+                src={step.imgPath}
+                alt={step.label}
+              />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+          >
+            Next
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
+    </Box>
+  );
+}
+
+export default SwipeableTextMobileStepper;
