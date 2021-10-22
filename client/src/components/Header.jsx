623ce7d89e55fba807f2d75c1698fd87baf19322
Raunak Singh <rksj2001@gmail.com>
2021-10-22 22:34:31 +0530

    Revert "Merge pull request #9 from rksingh2001/main"
    This reverts commit 2128aa848053a8f71f98d0027ca21f3340113428, reversing
    changes made to 33554b9b7d9d5bdd0bab13c559cf478e053b8e6e.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,26 +1,23 @@
 import React from "react";
-import { AppBar, Toolbar, Box, useTheme, useMediaQuery } from "@mui/material";
+import { AppBar, Toolbar, Box } from "@mui/material";
 
 import logo from "../assets/images/IMG_20211014_172013_181-removebg-preview.png";
 
 import Navigation from "./Navigation";
 
 const Header = () => {
-  const theme = useTheme();
-  const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
-
   return (
     <>
-      <AppBar position="absolute" color="inherit" gutterBottom>
-        <Toolbar variant="regular" sx={{ height: "15vh" }}>
-          <Box>
-            <img src={logo} alt="Logo" height={isSmall ? "40px" : "60px"} />
-          </Box>
-          <Box sx={{ flexGrow: 0.8 }}></Box>
-          <Navigation />
-        </Toolbar>
-      </AppBar>
-      <Box height="15vh"></Box>
+    <AppBar position="absolute" color="inherit" gutterBottom>
+      <Toolbar variant="regular" sx={{ height: "15vh" }}>
+        <Box>
+        <img src={logo} alt="Logo" height="60px" />
+        </Box>
+        <Box sx={{ flexGrow: 0.8 }}></Box>
+        <Navigation />
+      </Toolbar>
+    </AppBar>
+    <Box height="15vh"></Box>
     </>
   )
 }
