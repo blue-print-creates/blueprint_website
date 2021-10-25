8e9ba448bad7358a3b65332e5be25284801d9afe
Raunak Singh <rksj2001@gmail.com>
2021-10-25 17:53:52 +0530

    Trying making new branch

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -3,11 +3,11 @@ import React from "react";
 // Topic Wise Categories
 
 const Gallery = () => {
-    return(
-        <div className="gallery">
-            Gallery
-        </div>
-    )
+  return (
+    <div className="gallery">
+      Gallery
+    </div>
+  )
 }
 
 export default Gallery;
