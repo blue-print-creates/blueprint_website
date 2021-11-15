8716fa2c099165f1faae63b94f6805fefa1d4c9c
Rakshit Gupta <75110126+RakshitGupta621@users.noreply.github.com>
2021-11-15 17:30:32 +0530

    Add files via upload

--- client/src/api/youtube.js
+++ client/src/api/youtube.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const API_KEY = "AIzaSyDQXdxiDwqkR8Kq1k7VPUF_QLIbvTs-5Ho";
+const API_KEY = process.env.REACT_APP_API_KEY ;
 const API_URL = "https://www.googleapis.com/youtube/v3/playlistItems";
 
 export default axios.create({
   baseURL: API_URL,
   params: {
     part: 'snippet, contentDetails, id',
-    maxResults: 5,
+    maxResults: 12,
     playlistId: "PLMC9KNkIncKtPzgY-5rmhvj7fax8fdxoj",
     key: API_KEY,
   },
