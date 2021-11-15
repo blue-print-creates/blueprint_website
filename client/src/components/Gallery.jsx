ecafc3912a05420d5040ccb9b20634952ec4e611
Raunak Singh <rksj2001@gmail.com>
2021-11-15 16:39:13 +0530

    Stopped google from making cookies so that website works on websites where cookies are blocked

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -41,7 +41,7 @@ const Gallery = () => {
                         id={videoId}
                         width="100%"
                         height="250" 
-                        src={"https://www.youtube.com/embed/" + videoId} 
+                        src={"https://www.youtube-nocookie.com/embed/" + videoId} 
                         title="YouTube video player" 
                         frameBorder="0" 
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
