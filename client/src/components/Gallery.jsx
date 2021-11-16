8c6e377d7337395c76b81ebb66382de38e882f00
Raunak Singh <rksj2001@gmail.com>
2021-11-16 10:49:24 +0530

    added try block

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Box, Typography, Card, CardActionArea, CardContent, Grid} from "@mui/material";
+import { Box, Typography, Card, CardActionArea, CardContent, Grid } from "@mui/material";
 import youtubeApi from '../api/youtube.js';
 
 const Gallery = () => {
@@ -13,6 +13,7 @@ const Gallery = () => {
 
   useEffect(() => {
     const getVideos = async keyword => {
+      try {
       const response = await youtubeApi.get();
       let videoArray = [];
       // eslint-disable-next-line array-callback-return
@@ -24,62 +25,65 @@ const Gallery = () => {
         })
       })
       setVideoIDArray(videoArray);
+      } catch (e) {
+        console.log(e)
+      }
     };
     getVideos();
   }, [])
-  
+
 
   return (
-      <Box className="gallery" alignItems="center">
-        <Grid style={{marginLeft: "-16px"}} container spacing={4} xs={12}>
-          {videoIDArray.map(({videoId, title, description}) => (
-            <Grid item key={videoId} xs={12} sm={6} md={4}>
-              <Card>
-                <CardActionArea >
-                    
-                      <iframe
-                        id={videoId}
-                        width="100%"
-                        height="250" 
-                        src={"https://www.youtube-nocookie.com/embed/" + videoId} 
-                        title="YouTube video player" 
-                        frameBorder="0" 
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                        allowFullScreen
-                        >
-                      </iframe>
-                  
-                  <CardContent >
-                      <Typography 
-                        style={{
-                          whiteSpace: "nowrap",
-                          overflow: "hidden",
-                          textOverflow: "ellipsis"
-                        }}
-                        variant="h6"
-                        component="div"
-                      >
-                      {title}
-                      </Typography>
-                        <Typography 
-                          style={{
-                            whiteSpace: "nowrap",
-                            overflow: "hidden",
-                            textOverflow: "ellipsis"
-                          }}
-                          variant="body2" 
-                          color="text.secondary"
-                        >
-                        {description}
-                      </Typography>
-                  </CardContent>
-                  
-                </CardActionArea>
-              </Card>
-            </Grid>
-          ))}
+    <Box className="gallery" alignItems="center">
+      <Grid style={{ marginLeft: "-16px" }} container spacing={4} xs={12}>
+        {videoIDArray.map(({ videoId, title, description }) => (
+          <Grid item key={videoId} xs={12} sm={6} md={4}>
+            <Card>
+              <CardActionArea >
+
+                <iframe
+                  id={videoId}
+                  width="100%"
+                  height="250"
+                  src={"https://www.youtube-nocookie.com/embed/" + videoId}
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                >
+                </iframe>
+
+                <CardContent >
+                  <Typography
+                    style={{
+                      whiteSpace: "nowrap",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis"
+                    }}
+                    variant="h6"
+                    component="div"
+                  >
+                    {title}
+                  </Typography>
+                  <Typography
+                    style={{
+                      whiteSpace: "nowrap",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis"
+                    }}
+                    variant="body2"
+                    color="text.secondary"
+                  >
+                    {description}
+                  </Typography>
+                </CardContent>
+
+              </CardActionArea>
+            </Card>
           </Grid>
-        </Box>
+        ))}
+      </Grid>
+    </Box>
   )
 }
 
