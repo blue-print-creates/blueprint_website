62b3b34124fceafcb18e0905926ac05025107ce3
Rakshit Gupta <75110126+RakshitGupta621@users.noreply.github.com>
2021-12-09 22:37:26 +0530

    Update youtube.js

--- client/src/api/youtube.js
+++ client/src/api/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = "PLrOFa8sDv6jfNxnFmp3N2bK0eqRkUPIxS";
+const API_KEY = "AIzaSyDQXdxiDwqkR8Kq1k7VPUF_QLIbvTs-5Ho";
 const API_URL = "https://www.googleapis.com/youtube/v3/playlistItems";
 
 export default axios.create({
@@ -8,7 +8,7 @@ export default axios.create({
   params: {
     part: 'snippet, contentDetails, id',
     maxResults: 12,
-    playlistId: "PLMC9KNkIncKtPzgY-5rmhvj7fax8fdxoj",
+    playlistId: "PLrOFa8sDv6jfNxnFmp3N2bK0eqRkUPIxS",
     key: API_KEY,
   },
   headers: {}
